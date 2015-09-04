7427b458a8803cb8725df8a2eb32a3e89352933f
unknown <pankaj.shrestha@LT-TEK15-09.tekcenture.local>
2015-09-04 14:08:00 -0500

    copied from : https://code.msdn.microsoft.com/Async-programming-using-ec0347a1

---
+++ Async programming using Tasks%2c Async and Await in C%23/C#/TaskAsyncAwaitSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskAsyncAwaitSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running Async code...");
+            
+            Task<bool> result = DoWorkAsyc();
+            Console.WriteLine("Async method called.\nResult will display once available from the async method.");
+
+            result.Wait();
+
+            Console.WriteLine(result.Result);
+            
+            Console.WriteLine("Press any key to exit: ");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// optimized DoWork method - runs aync!
+        /// </summary>
+        public static async Task<bool> DoWorkAsyc()
+        {
+            // use await here, like so:
+            return await Task.Run(() => ProcessJob());
+        }
+
+        private static bool ProcessJob()
+        {
+            //long running work code goes here
+            Thread.Sleep(5000);
+
+            return true;
+        }
+    }
+}
